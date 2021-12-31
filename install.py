76edd24fcd63e6598b671be04649c8ff7be4b3ec
C1510 <chrismingard@gmail.com>
2021-12-31 17:22:35 +0000

    yay

---
+++ install.py
@@ -0,0 +1,14 @@
+import sys, os, subprocess
+
+# implement pip as a subprocess:
+package = 'opencv-python'
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', f'{package}'])
+
+if not os.path.isdir(f'imgs_np/'):
+    os.makedirs(f'imgs_np/')
+if not os.path.isdir(f'imgs/'):
+    os.makedirs(f'imgs/')
+if not os.path.isdir(f'imgs_classified/'):
+    os.makedirs(f'imgs_classified/')
+if not os.path.isdir(f'imgs_rectangled/'):
+    os.makedirs(f'imgs_rectangled/')
--- lib/install.py
+++
@@ -1,6 +0,0 @@
-import sys
-import subprocess
-
-# implement pip as a subprocess:
-package = 'opencv-python'
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', f'{package}'])
