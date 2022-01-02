2c0987f31e508e98b16639fe0eca3fe308a33189
C1510 <chrismingard@gmail.com>
2022-01-02 15:34:40 +0000

    yay

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ if __name__=='__main__':
     # This prints the number of fossils that were found
     print(stats.shape[0],'fossils were found')
 
-    np.savetxt(f'imgs_rectangled/{img_name.split(".")[0]}.txt', stats, fmt='%.4e', delimiter=' ', header='left_top_x left_top_y x_length y_length vol',comments='')
+    np.savetxt(f'imgs_rectangled/{img_name.split(".")[0]}.txt', stats, fmt='%.0f', delimiter=' ', header='left_top_x left_top_y x_length y_length vol',comments='')
 
 
 
