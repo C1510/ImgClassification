5f1062aaeb4272973b754dddcf8cd8a0e6687274
C1510 <chrismingard@gmail.com>
2022-03-05 08:03:49 +0000

    yay

--- neural_network/train.py
+++ neural_network/train.py
@@ -65,13 +65,35 @@ classes=dict(zip(classes_list,[i for i in range(len(classes_list))]))
 import torch.nn as nn
 import torch.nn.functional as F
 
+# class Net(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#         self.conv1 = nn.Conv2d(1, 96, (9, 9), (2, 2))
+#         self.pool = nn.MaxPool2d(3, 3)
+#         self.conv2 = nn.Conv2d(96, 12, (3, 3))
+#         self.pool2 = nn.MaxPool2d(2, 2)
+#         self.fc1 = nn.Linear(12*6*6, 120)
+#         self.fc2 = nn.Linear(120, 84)
+#         self.fc3 = nn.Linear(84, num_classes)
+#
+#     def forward(self, x):
+#         x = self.pool(F.relu(self.conv1(x)))
+#         x = self.pool2(F.relu(self.conv2(x)))
+#         x = x.view(-1, 12*6*6)
+#         x = F.relu(self.fc1(x))
+#         x = F.relu(self.fc2(x))
+#         x = self.fc3(x)
+#         return x
+
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 96, (9, 9), (2, 2))
+        self.conv1 = nn.Conv2d(1, 48, (5, 5), (1, 1))
         self.pool = nn.MaxPool2d(3, 3)
-        self.conv2 = nn.Conv2d(96, 12, (3, 3))
+        self.conv2 = nn.Conv2d(48, 12, (3, 3))
         self.pool2 = nn.MaxPool2d(2, 2)
+        self.conv3 = nn.Conv2d(12, 12, (3, 3))
+        self.pool3 = nn.MaxPool2d(2, 2)
         self.fc1 = nn.Linear(12*6*6, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, num_classes)
@@ -79,6 +101,8 @@ class Net(nn.Module):
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool2(F.relu(self.conv2(x)))
+        x = self.pool3(F.relu(self.conv3(x)))
+        print(x.shape)
         x = x.view(-1, 12*6*6)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
